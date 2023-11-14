01c38a30fb6fcacf320a51a0361dc01044c2145e
SCUTys <129161627+SCUTys@users.noreply.github.com>
2023-11-14 18:10:53 +0800

    重构并优化了更新逻辑，但有一个硬伤是医生改名字的话刷新完出不来（realname在子函数里没法改））

--- doctor_application.py
+++ doctor_application.py
@@ -4,81 +4,89 @@ from tkinter import messagebox
 from tkinter.ttk import Treeview
 import sqlite3
 
-def fetch_data(realname):
-    try:
-        conn = sqlite3.connect('hospital_database.db')
-        cursor = conn.cursor()
-
-        cursor.execute(f"SELECT * FROM Doctors WHERE doctor_name='{realname}'")
-        doctors_data = cursor.fetchall()
-        print("Doctors data:")
-        for row in doctors_data:
-            print(row)
-
-        cursor.execute(f"SELECT * FROM Treatments WHERE doctor_id IN (SELECT doctor_id FROM Doctors WHERE doctor_name='{realname}')")
-        treatment_data = cursor.fetchall()
-        print("Treatment data:")
-        for row in treatment_data:
-            print(row)
-
-        conn.close()
-
-        return doctors_data, treatment_data
-    except sqlite3.Error as e:
-        print(e)
-
-
-def refresh_data(realname, text_area_doctors, text_area_treatment):
-    doctors_data, treatment_data = fetch_data(realname)
-
-    text_area_doctors.delete('1.0', tk.END)
-    for row in doctors_data:
-        text_area_doctors.insert(tk.INSERT, ' | '.join(map(str, row)) + '\n')
-
-    text_area_treatment.delete('1.0', tk.END)
-    for row in treatment_data:
-        text_area_treatment.insert(tk.INSERT, ' | '.join(map(str, row)) + '\n')
-
-
-def open_update_window(doctors_data):
-    update_window = tk.Toplevel()
-
-    doctor_id_entry = tk.Entry(update_window)
-    doctor_id_entry.insert(0, doctors_data[0][0])
-    doctor_id_entry.grid(column=0, row=0)
-
-    doctor_name_entry = tk.Entry(update_window)
-    doctor_name_entry.insert(0, doctors_data[0][1])
-    doctor_name_entry.grid(column=1, row=0)
-
-    department_id_entry = tk.Entry(update_window)
-    department_id_entry.insert(0, doctors_data[0][2])
-    department_id_entry.grid(column=2, row=0)
-
-    update_button = tk.Button(update_window, text="Update", command=lambda: update_data(doctor_id_entry.get(), doctor_name_entry.get(), department_id_entry.get()))
-    update_button.grid(column=3, row=0)
-
-def update_data(doctor_id, doctor_name, department_id):
-    conn = sqlite3.connect('hospital_database.db')
-    cursor = conn.cursor()
-
-    cursor.execute(f"UPDATE Doctors SET doctor_name='{doctor_name}', department_id='{department_id}' WHERE doctor_id='{doctor_id}'")
-    cursor.execute(f"UPDATE treatment SET doctor_id='{doctor_id}' WHERE doctor_id='{doctor_id}'")
-
-    conn.commit()
-    conn.close()
-
-
-def update_data(realname):
-    doctors_data, treatment_data = fetch_data(realname)
 
 def doctor_application_entry_window(realname):
+
+    r = realname
     root = tk.Tk()
     root.title("Main Application")
 
     #一个医生有多个patient和唯一的department
     #医生可以注册他们的唯一帐户，但需要经过管理员的验证。医生可以修改他们的个人信息，但不能修改他们的科室信息。
-    #main文件Line207左右
+    #main文件Line207
+
+    def fetch_data(realname):
+        try:
+            conn = sqlite3.connect('hospital_database.db')
+            cursor = conn.cursor()
+
+            cursor.execute(f"SELECT * FROM Doctors WHERE doctor_name='{realname}'")
+            doctors_data = cursor.fetchall()
+            print("Doctors data:")
+            for row in doctors_data:
+                print(row)
+
+            cursor.execute(
+                f"SELECT * FROM Treatments WHERE doctor_id IN (SELECT doctor_id FROM Doctors WHERE doctor_name='{realname}')")
+            treatment_data = cursor.fetchall()
+            print("Treatment data:")
+            for row in treatment_data:
+                print(row)
+
+            conn.close()
+
+            return doctors_data, treatment_data
+        except sqlite3.Error as e:
+            print(e)
+
+    def refresh_data(realname, text_area_doctors, text_area_treatment):
+        doctors_data, treatment_data = fetch_data(realname)
+
+        text_area_doctors.delete('1.0', tk.END)
+        for row in doctors_data:
+            text_area_doctors.insert(tk.INSERT, ' | '.join(map(str, row)) + '\n')
+
+        text_area_treatment.delete('1.0', tk.END)
+        for row in treatment_data:
+            text_area_treatment.insert(tk.INSERT, ' | '.join(map(str, row)) + '\n')
+
+    def open_update_window(realname, doctors_data):
+        update_window = tk.Toplevel()
+
+        doctor_id_entry = tk.Entry(update_window)
+        doctor_id_entry.insert(0, doctors_data[0][0])
+        doctor_id_entry.grid(column=0, row=0)
+
+        doctor_name_entry = tk.Entry(update_window)
+        doctor_name_entry.insert(0, doctors_data[0][1])
+        doctor_name_entry.grid(column=1, row=0)
+
+        department_id_entry = tk.Entry(update_window)
+        department_id_entry.insert(0, doctors_data[0][2])
+        department_id_entry.grid(column=2, row=0)
+
+        update_button = tk.Button(update_window, text="Update",
+                                  command=lambda: update_data(realname, doctor_id_entry.get(), doctor_name_entry.get(),
+                                                              department_id_entry.get()))
+        update_button.grid(column=3, row=0)
+
+    def update_data(realname, doctor_id, doctor_name, department_id):
+        conn = sqlite3.connect('hospital_database.db')
+        cursor = conn.cursor()
+        doctors_data, treatment_data = fetch_data(realname)
+        change_doctor_id = True if doctor_id != doctors_data[0] else False
+        change_doctor_name = True if doctor_name != doctors_data[1] else False
+
+        if(change_doctor_id):
+            cursor.execute(f"UPDATE Doctors SET doctor_id='{doctor_id}' WHERE doctor_id='{doctors_data[0]}'")
+            cursor.execute(f"UPDATE Treatments SET doctor_id='{doctor_id}' WHERE doctor_id='{doctors_data[0]}'")
+        if(change_doctor_name):
+            cursor.execute(f"UPDATE Doctors SET doctor_name='{doctor_name}' WHERE doctor_name='{doctors_data[1]}'")
+            cursor.execute(f"UPDATE Login SET realname='{doctor_name}' WHERE realname='{doctors_data[1]}' AND access_level=2")
+
+        conn.commit()
+        conn.close()
+
 
     doctors_data, treatment_data = fetch_data(realname)
 
@@ -91,9 +99,11 @@ def doctor_application_entry_window(realname):
     tk.Label(text="Treatment Data").grid(column=1, row=0)
     text_area_treatment = scrolledtext.ScrolledText(width=40, height=10)
     text_area_treatment.grid(column=1, row=1)
-    text_area_treatment.insert(tk.INSERT, treatment_data)
+    for row in treatment_data:
+        text_area_treatment.insert(tk.INSERT, ' | '.join(map(str, row)) + '\n')
 
-    update_button = tk.Button(root, text="Update Information", command=lambda: open_update_window(doctors_data))
+    update_button = tk.Button(root, text="Update Information", 
+                              command=lambda: open_update_window(realname, doctors_data))
     update_button.grid(column=0, row=2)
 
     refresh_button = tk.Button(root, text="Refresh",
