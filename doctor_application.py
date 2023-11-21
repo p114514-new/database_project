c6c2d0332efddd78c674c770aa4b5667718688cf
SCUTys <129161627+SCUTys@users.noreply.github.com>
2023-11-21 15:35:00 +0800

    改了界面结构，由于我数据库炸了不知道刷新的问题修了没，谁没事跑跑呗

--- doctor_application.py
+++ doctor_application.py
@@ -5,9 +5,9 @@ from tkinter.ttk import Treeview
 import sqlite3
 
 
-def doctor_application_entry_window(realname):
+def doctor_application_entry_window(realnames):
 
-    r = realname
+    realname = realnames
     root = tk.Tk()
     root.title("Main Application")
 
@@ -39,16 +39,13 @@ def doctor_application_entry_window(realname):
         except sqlite3.Error as e:
             print(e)
 
-    def refresh_data(realname, text_area_doctors, text_area_treatment):
+    def refresh_data(realname, text_area_doctors):
         doctors_data, treatment_data = fetch_data(realname)
 
         text_area_doctors.delete('1.0', tk.END)
         for row in doctors_data:
             text_area_doctors.insert(tk.INSERT, ' | '.join(map(str, row)) + '\n')
 
-        text_area_treatment.delete('1.0', tk.END)
-        for row in treatment_data:
-            text_area_treatment.insert(tk.INSERT, ' | '.join(map(str, row)) + '\n')
 
     def open_update_window(realname, doctors_data):
         update_window = tk.Toplevel()
@@ -87,27 +84,112 @@ def doctor_application_entry_window(realname):
         conn.commit()
         conn.close()
 
+    def personal_info_window(realname):
+        personal_info = tk.Toplevel()
+        personal_info.title("Personal Information")
 
-    doctors_data, treatment_data = fetch_data(realname)
+        doctors_data, treatment_data = fetch_data(realname)
 
-    tk.Label(text="Doctors Data").grid(column=0, row=0)
-    text_area_doctors = scrolledtext.ScrolledText(width=40, height=5)
-    text_area_doctors.grid(column=0, row=1)
-    for row in doctors_data:
-        text_area_doctors.insert(tk.INSERT, ' | '.join(map(str, row)) + '\n')
+        tk.Label(personal_info, text="Doctors Data").grid(column=0, row=0)
+        text_area_doctors = scrolledtext.ScrolledText(personal_info, width=40, height=5)
+        text_area_doctors.grid(column=0, row=1)
+        for row in doctors_data:
+            text_area_doctors.insert(tk.INSERT, ' | '.join(map(str, row)) + '\n')
 
-    tk.Label(text="Treatment Data").grid(column=1, row=0)
-    text_area_treatment = scrolledtext.ScrolledText(width=40, height=10)
-    text_area_treatment.grid(column=1, row=1)
-    for row in treatment_data:
-        text_area_treatment.insert(tk.INSERT, ' | '.join(map(str, row)) + '\n')
+        update_button = tk.Button(personal_info, text="Update Information",
+                                  command=lambda: open_update_window(realname, doctors_data))
+        update_button.grid(column=0, row=2)
 
-    update_button = tk.Button(root, text="Update Information", 
-                              command=lambda: open_update_window(realname, doctors_data))
-    update_button.grid(column=0, row=2)
+        refresh_button = tk.Button(personal_info, text="Refresh",
+                                   command=lambda: refresh_data(realname, text_area_doctors))
+        refresh_button.grid(column=0, row=3)
 
-    refresh_button = tk.Button(root, text="Refresh",
-                               command=lambda: refresh_data(realname, text_area_doctors, text_area_treatment))
-    refresh_button.grid(column=0, row=3)
+    def workbench_window(realname):
+        workbench = tk.Toplevel()
+        workbench.title("Workbench")
+
+        doctors_data, treatment_data = fetch_data(realname)
+
+        tk.Label(workbench, text="Treatment Data").grid(column=0, row=0)
+        text_area_treatment = scrolledtext.ScrolledText(workbench, width=40, height=10)
+        text_area_treatment.grid(column=0, row=1)
+        for row in treatment_data:
+            text_area_treatment.insert(tk.INSERT, ' | '.join(map(str, row)) + '\n')
+
+        # 显示病人信息
+        tk.Label(workbench, text="Patient Data").grid(column=1, row=0)
+        text_area_patient = scrolledtext.ScrolledText(workbench, width=40, height=10)
+        text_area_patient.grid(column=1, row=1)
+
+        # 添加从数据库获取病人信息的代码，并显示在text_area_patient中
+        def restore_patient(textbox):
+            conn = sqlite3.connect('hospital_database.db')
+            cursor = conn.cursor()
+
+            cursor.execute(f"""
+                SELECT Patients.*
+                FROM Patients
+                INNER JOIN Treatments ON Patients.patient_id = Treatments.patient_id
+                INNER JOIN Doctors ON Treatments.doctor_id = Doctors.doctor_id
+                WHERE Doctors.doctor_name = '{realname}'
+            """)
+
+            patient_data = cursor.fetchall()
+
+            textbox.delete('1.0', tk.END)
+            for row in patient_data:
+                textbox.insert(tk.INSERT, ' | '.join(map(str, row)) + '\n')
+
+            conn.close()
+
+        restore_patient(text_area_patient)
+
+        # 输入框和按钮
+        patient_name_entry = tk.Entry(workbench)
+        patient_name_entry.grid(column=0, row=2)
+
+        def search_patient(name, textbox):
+            conn = sqlite3.connect('hospital_database.db')
+            cursor = conn.cursor()
+
+            cursor.execute(f"SELECT * FROM Patients WHERE patient_name='{name}'")
+
+            patient_data = cursor.fetchall()
+
+            textbox.delete('1.0', tk.END)
+            for row in patient_data:
+                textbox.insert(tk.INSERT, ' | '.join(map(str, row)) + '\n')
+
+            conn.close()
+
+
+        def refresh_data_workbench(realname, text_area_patient, text_area_treatment):
+            text_area_patient.delete('1.0', tk.END)
+            text_area_treatment.delete("1.0", tk.END)
+            restore_patient(text_area_patient)
+
+            doctors_data, treatment_data = fetch_data(realname)
+            for row in treatment_data:
+                text_area_treatment.insert(tk.INSERT, ' | '.join(map(str, row)) + '\n')
+
+
+        search_button = tk.Button(workbench, text="Search",
+                                  command=lambda: search_patient(patient_name_entry.get(), text_area_patient))
+        search_button.grid(column=1, row=2)
+
+        restore_button = tk.Button(workbench, text="Restore",
+                                   command=lambda: restore_patient(text_area_patient))
+        restore_button.grid(column=2, row=2)
+
+        refresh_button = tk.Button(workbench, text="Refresh",
+                                   command=lambda: refresh_data_workbench(realname, text_area_patient, text_area_treatment))
+        refresh_button.grid(column=3, row=2)
+
+
+    personal_info_button = tk.Button(root, text="Personal Information", command=lambda: personal_info_window(realname))
+    personal_info_button.pack()
+
+    workbench_button = tk.Button(root, text="Workbench", command=lambda: workbench_window(realname))
+    workbench_button.pack()
 
     root.mainloop()
